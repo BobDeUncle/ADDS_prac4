e66c8c69810b5ee10b01576904bc18171a5d8b72
Jesse Mansbridge <jessemansbridge@hotmail.com>
2022-04-02 17:08:54 +1030

    Edge cases

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -13,6 +13,8 @@ int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
 
   if (numCrates <= 0 || loadSize <= 0) {
     return -1;
+  } else if (numCrates == 0 || loadSize == 0) {
+    return 0;
   } else if (numCrates <= loadSize) {
     return 1;
   } else {
